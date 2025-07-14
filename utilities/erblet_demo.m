485f8fba60c9ed02665fce6efdfea73e341c0455
benrabin <101032246+benrabin@users.noreply.github.com>
2025-07-14 00:11:44 -0700

    add transform_audio.py utility and erblet demo

---
+++ utilities/erblet_demo.m
@@ -0,0 +1,17 @@
+function [c, fc] = erblet_demo(path)
+    [f, fs] = audioread(path);
+    [g, a, fc] = audfilters(fs, length(f), 'fractional');
+    c = filterbank(f, {'realdual', g}, a);
+
+    r = 2*real(ifilterbank(c, g, a));
+    err = norm(f-r);
+    if err > 1e-10
+        warning('Reconstruction error greater than 1e-10.\nError: %f', err)
+    end
+
+    figure;
+    plotfilterbank(c, a, fc, fs, 60, 'audtick');
+    colorbar;
+    title('ERBlet Filterbank Spectrogram');
+    xlabel('Time (Sec)');
+    ylabel('Center Frequency (Hz)'); 
---
+++ utilities/transform_audio.py
@@ -0,0 +1,66 @@
+import numpy as np
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+import pywt
+import os
+
+
+USE_MATLAB = True # required for Erblet transforms
+if USE_MATLAB:
+    import matlab.engine 
+    eng = matlab.engine.start_matlab()
+else:
+    eng = None
+
+def erblet_demo(file_path):
+    if not USE_MATLAB:
+        raise NotImplementedError('MATLAB is required to perform Erblet transforms')
+    
+    c, fc = eng.erblet_demo(file_path, nargout=2)
+    coefs = [np.array(ci)[:, 0] for ci in c]
+    freqs = np.array(fc)[:, 0]
+
+    return coefs, freqs
+
+def cwt_file(file_path, wavelet='cmor1.5-1.0', low_freq=80, high_freq=8000, num_scales=100, 
+             visualize=False, title='CWT with Morlet Wavelet'):
+    rate, signal = wavfile.read(file_path)
+    frequencies = np.logspace(np.log10(low_freq), np.log10(high_freq), num_scales)
+    scales = pywt.frequency2scale(wavelet, frequencies / rate)
+    coefs, freqs = pywt.cwt(signal, scales, wavelet, 1/rate)
+
+    if visualize:
+        plt.figure(figsize=(10, 6))
+        plt.imshow(
+            np.abs(coefs),
+            aspect='auto',
+            cmap='jet',
+            extent=[0, len(signal)/rate, low_freq, high_freq]
+        )
+        plt.colorbar(label='Magnitude')
+        # plt.yscale('log')
+        plt.ylabel('Frequency (Hz)')
+        plt.xlabel('Time (s)')
+        plt.title(title)
+        plt.show()
+
+    return coefs, freqs
+
+def fft_file(file_path, discard_negative=True, visualize=False, title='Fourier Spectrum'):
+    rate, signal = wavfile.read(file_path)
+    coefs = np.fft.fft(signal)
+    freqs = np.fft.fftfreq(len(signal), 1/rate)
+    if discard_negative:
+        negative_idx = len(freqs) // 2
+        coefs = coefs[:negative_idx]
+        freqs = freqs[:negative_idx]
+
+    if visualize:
+        plt.plot(freqs, np.abs(coefs), lw=.1)
+        plt.xlabel('Frequency (Hz)')
+        plt.ylabel('Magnitude')
+        plt.yscale('log')
+        plt.title(title)
+        plt.show()
+    
+    return coefs, freqs
