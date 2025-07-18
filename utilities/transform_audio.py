9d67979a178afd1c9cb3a600044215907169fea4
benrabin <101032246+benrabin@users.noreply.github.com>
2025-07-18 14:12:52 -0700

    add transform_list

--- utilities/transform_audio.py
+++ utilities/transform_audio.py
@@ -3,6 +3,7 @@ from scipy.io import wavfile
 import matplotlib.pyplot as plt
 import pywt
 import librosa
+from tqdm.notebook import tqdm
 
 
 USE_MATLAB = True # required for Erblet transforms
@@ -21,6 +22,7 @@ def erblet_file(file_path, visualize=False):
     freqs = np.array(fc)[:, 0]
 
     return coefs, freqs
+erblet_file.affix = 'erb'
 
 def cwt_file(file_path, wavelet='cmor1.5-1.0', low_freq=80, high_freq=8000, num_scales=100, 
              visualize=False, title='CWT with Morlet Wavelet'):
@@ -40,6 +42,7 @@ def cwt_file(file_path, wavelet='cmor1.5-1.0', low_freq=80, high_freq=8000, num_
         plt.show()
 
     return coefs, freqs
+cwt_file.affix = 'cwt'
 
 def stft_file(file_path, n_fft=1024, visualize=False, title='Log-Frequency Spectrogram (STFT Magnitude)'):
     signal, rate = librosa.load(file_path, sr=None)
@@ -54,11 +57,12 @@ def stft_file(file_path, n_fft=1024, visualize=False, title='Log-Frequency Spect
         plt.show()
 
     return coefs, freqs
+stft_file.affix = 'stft'
 
-def fft_file(file_path, discard_negative=True, visualize=False, title='Fourier Spectrum'):
+def fft_file(file_path, n=None, discard_negative=True, visualize=False, title='Fourier Spectrum'):
     rate, signal = wavfile.read(file_path)
-    coefs = np.fft.fft(signal)
-    freqs = np.fft.fftfreq(len(signal), 1/rate)
+    coefs = np.fft.fft(signal, n)
+    freqs = np.fft.fftfreq(len(signal) if n is None else n, 1/rate)
     if discard_negative:
         negative_idx = len(freqs) // 2
         coefs = coefs[:negative_idx]
@@ -73,3 +77,26 @@ def fft_file(file_path, discard_negative=True, visualize=False, title='Fourier S
         plt.show()
     
     return coefs, freqs
+fft_file.affix = 'fft'
+
+def transform_list(transform_file, file_list, file_names, *args, progress=True, affix=None, **kwargs):
+    first_freqs = None
+    def check_freq(transform):
+        nonlocal first_freqs
+        coefs, freqs = transform
+        if first_freqs is None:
+            first_freqs = freqs
+        else:
+            assert np.array_equal(freqs, first_freqs), 'frequency scales are not consistent across files'
+        return coefs
+    
+    if affix is None:
+        affix = getattr(transform_file, 'affix', 'xform')
+    transformed_names = [name.rsplit('.', 1)[0] + '_' + affix for name in file_names]
+
+    coefs_gen = (check_freq(transform_file(f, *args, **kwargs)) for f in file_list)
+    if progress:
+        coefs_gen = tqdm(coefs_gen, total=len(file_list))
+
+    np.savez(affix + '_coefs', **dict(zip(transformed_names, coefs_gen)))
+    np.save(affix + '_freqs', first_freqs)
