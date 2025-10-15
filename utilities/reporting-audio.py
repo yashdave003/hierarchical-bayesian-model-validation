dc4fcfbe17b8a59f60135498f47d0619ed1b3336
henry-liev <henryliev@proton.me>
2025-10-15 14:40:07 -0700

    ran some pipelines and working on categorization of results

---
+++ utilities/reporting-audio.py
@@ -0,0 +1,165 @@
+import os
+import git
+from pathlib import Path
+from typing import List
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+import plotly.express as px
+import ipywidgets as widgets
+from IPython.display import display
+import numpy as np
+import plotly.graph_objects as go
+from IPython.display import clear_output
+import scipy
+from scipy import stats
+from scipy.spatial import ConvexHull
+import pylustrator
+from scipy.spatial import Delaunay
+from scipy.spatial import distance
+from sklearn.decomposition import PCA
+from scipy.spatial import Delaunay
+
+ROOT_DIR = Path(git.Repo('.', search_parent_directories=True).working_tree_dir)
+RERUN=False
+DPI=100
+
+def variance_prior(r, eta, scale=1):
+    beta = (eta+1.5)/r
+    var_prior = scale * scipy.special.gamma(beta + 1/r)/scipy.special.gamma(beta)
+    return var_prior
+
+def kurtosis_prior(r, eta, fisher=True):
+    beta = (eta+1.5)/r
+    kurtosis = 3*scipy.special.gamma(beta + 2/r)*scipy.special.gamma(beta)/scipy.special.gamma(beta+1/r)**2 
+    if fisher:
+        return kurtosis - 3
+    else:
+        return kurtosis 
+
+def find_master_dfs(root_dir: str) -> List[str]:
+    root_path = Path(root_dir)
+    if not root_path.exists():
+        raise FileNotFoundError(f"Directory not found: {root_dir}")
+
+    master_df_paths = []
+    for current_dir, _, files in os.walk(root_path):
+        if 'master_df.csv' in files:
+            master_df_path = Path(os.path.join(current_dir, 'master_df.csv'))
+            master_df_paths.append(str(master_df_path.absolute()))
+    return master_df_paths
+
+def line_intersects_hull(hull):
+
+    x_vals = np.linspace(0, 20, 1000)
+    eta_vals = 1.5 + np.zeros_like(x_vals)
+    roi_beta = (eta_vals) / x_vals
+    roi = 1 / roi_beta
+    
+    if hull is None or hull is np.nan:
+        return 0
+    line_points = np.column_stack((x_vals, roi))
+    return int(np.any(in_hull(line_points, hull)))
+
+def add_hull_and_kurt(master_df, rEtaKsstats_dict, GROUP='group', debug=False):
+
+    master_df_copy = master_df.copy()
+    master_df_copy = master_df.set_index(GROUP)
+    groups = master_df_copy.index
+    master_df_copy["hull"] = ""
+
+    for group in groups:
+        obs_kurt, kurt_lower, kurt_upper = master_df_copy.loc[group, "obs_kurt"], master_df_copy.loc[group, "kurt_lower"], master_df_copy.loc[group, "kurt_upper"]
+        best_scale = master_df_copy.loc[group, "best_scale"]
+
+        if master_df_copy.loc[group, "total_samples"] < 10:
+            master_df_copy.loc[group, "hull"] = np.nan
+        
+        else:
+            drop_keys =list(rEtaKsstats_dict[group].keys())[-3:]
+            if debug:
+                print(drop_keys)
+            pre_optimization = pd.DataFrame(rEtaKsstats_dict[group]).drop(drop_keys, axis = 1 )
+            optimization = pd.DataFrame(rEtaKsstats_dict[group])[drop_keys]
+            optimization = optimization.rename(columns = {"r_optimize": "r", "eta_optimize": "eta", drop_keys[-1]: "ksstat"})
+            optimization = optimization.dropna()
+            full_df = pre_optimization.merge(optimization, on=["r", "eta"], how="outer")
+            full_df['kurt'] = full_df.apply(lambda row : kurtosis_prior(r=row['r'], eta=row['eta'], scale=best_scale), axis=1)
+            full_df['pass_kurt'] = (full_df['kurt'] > kurt_lower) & (full_df['kurt'] < kurt_upper)
+            kurt_df = full_df.copy()
+            full_df = full_df.set_index(["r", "eta"])
+            full_df["ksstat"] = full_df.min(axis=1)
+            full_df = full_df.reset_index()
+            full_df = full_df[["r", "eta", "ksstat"]]
+            full_df["1/beta"] = full_df["r"]/(full_df["eta"] + 1.5)
+            MULT = 1.2
+            cutoff = max(min(full_df["ksstat"]) * MULT, 0.01)
+            filtered_df = full_df[full_df["ksstat"] < cutoff]
+            points = np.column_stack((filtered_df["r"], filtered_df["1/beta"])) + stats.norm.rvs(size=(len(filtered_df), 2)) * 0.001  # Adding small noise for convex hull computation
+            if len(points) < 3:
+                hull=np.nan
+            else:
+                hull = ConvexHull(points)
+            master_df_copy.loc[group, "hull"] = hull
+
+            master_df_copy.loc[group, "pass_kurt_anywhere"] = np.any(kurt_df['pass_kurt'] > 0)
+            master_df_copy.loc[group, "pass_kurt_intersect_hull"] = np.any((kurt_df['pass_kurt'] == 1) & (kurt_df['ksstat'] < cutoff))
+
+    return master_df_copy.reset_index()
+
+def in_hull(p, hull):
+    if hasattr(hull, 'vertices') and not isinstance(hull, Delaunay):
+        hull = Delaunay(hull.points)
+    elif not isinstance(hull, Delaunay):
+        hull = Delaunay(hull)
+    return hull.find_simplex(p) >= 0
+
+def add_hull(master_df, rEtaKsstats_dict, GROUP='group', debug=False):
+
+    master_df_copy = master_df.copy()
+    master_df_copy = master_df.set_index(GROUP)
+    groups = master_df_copy.index
+    master_df_copy["hull"] = ""
+
+    for group in groups:
+        if master_df_copy.loc[group, "total_samples"] < 10:
+            master_df_copy.loc[group, "hull"] = np.nan
+           
+        else:
+            drop_keys =list(rEtaKsstats_dict[group].keys())[-3:]
+            if debug:
+                print(drop_keys)
+            pre_optimization = pd.DataFrame(rEtaKsstats_dict[group]).drop(drop_keys, axis = 1 )
+            optimization = pd.DataFrame(rEtaKsstats_dict[group])[drop_keys]
+            optimization = optimization.rename(columns = {"r_optimize": "r", "eta_optimize": "eta", drop_keys[-1]: "ksstat"})
+            optimization = optimization.dropna()
+            full_df = pre_optimization.merge(optimization, on=["r", "eta"], how="outer")
+            full_df = full_df.set_index(["r", "eta"])
+            full_df["ksstat"] = full_df.min(axis=1)
+            full_df = full_df.reset_index()
+            full_df = full_df[["r", "eta", "ksstat"]]
+            full_df["1/beta"] = full_df["r"]/(full_df["eta"] + 1.5)
+            MULT = 1.2
+            cutoff = max(min(full_df["ksstat"]) * MULT, 0.01)
+            filtered_df = full_df[full_df["ksstat"] < cutoff]
+            points = np.column_stack((filtered_df["r"], filtered_df["1/beta"])) + stats.norm.rvs(size=(len(filtered_df), 2)) * 0.001  # Adding small noise for convex hull computation
+            if len(points) < 3:
+                hull=np.nan
+            else:
+                hull = ConvexHull(points)
+            master_df_copy.loc[group, "hull"] = hull
+
+    return master_df_copy.reset_index()
+
+save_path_with_hull = Path(os.path.join(ROOT_DIR, 'publication', 'paper', 'CSVs', 'final_results_audio_with_hull.pickle'))
+save_path_without_hull = Path(os.path.join(ROOT_DIR, 'publication', 'paper', 'CSVs', 'final_results_audio.csv'))
+
+if RERUN or save_path_without_hull.exists():
+    main_df = pd.read_csv(save_path_without_hull).drop("Unnamed: 0", axis=1)
+    main_df = main_df[main_df['total_samples'] > 100]
+    main_df['best_beta'] = (main_df['best_eta'] + 1.5)/main_df['best_r']
+    main_df = main_df#[(main_df['dataset'] != 'standardTesting')]
+else:
+    print("Run make_results_audio_csv.ipynb first")
+
