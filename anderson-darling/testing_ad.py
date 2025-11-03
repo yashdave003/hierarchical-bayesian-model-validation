e6876449254c2647a8153a63140c24b2e545ca14
benrabin <101032246+benrabin@users.noreply.github.com>
2025-11-02 21:16:17 -0800

    anderson-darling intersection testing

---
+++ anderson-darling/testing_ad.py
@@ -0,0 +1,33 @@
+import scipy
+import numpy as np
+
+def compute_A2(x, F, presorted=True, eps=1e-16):
+    '''
+    x.shape = (n, N) 
+        n = sample size
+        N = # of samples
+    F = cdf of the H_0 distribution
+    '''
+    if not presorted:
+        x = np.sort(x, axis=0)
+    z = np.clip(F(x), eps, 1 - eps)
+
+    n = x.shape[0]
+    i = np.arange(1, 2 * n, 2).reshape((n,) + (1,) * (len(x.shape) - 1))
+    S = np.sum(i * (np.log(z) + np.log(1 - z[::-1])), axis=0) / n
+    return -n - S
+
+# TODO: determine N automatically via stability condition
+@np.vectorize
+def evaluate_asymptotic_cdf(z, N=5):
+    '''
+    asymptotic cdf of A^2 under H_0 at z using first N terms in expansion
+    https://doi.org/10.1214/aoms/1177729437 (pp. 204)
+    '''
+    @np.vectorize
+    def term(j):
+        t_j = (4*j+1)**2 * np.pi**2 / (8*z)
+        integrand = lambda w: np.exp(z / (8*(1+w**2)) - w**2 * t_j)
+        integral = scipy.integrate.quad(integrand, 0, np.inf)[0]
+        return integral * scipy.special.binom(-.5, j) * (4*j+1) * np.exp(-t_j)
+    return np.sqrt(2 * np.pi) / z * np.sum(term(np.arange(N)))
